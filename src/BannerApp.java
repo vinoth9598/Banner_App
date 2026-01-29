8e43f349ebcfe645cf9ecf284c2f8a2dadd5eaeb
vinoth9598 <pvinoth9598@gmail.com>
2026-01-29 16:37:12 +0530

    [vinoth] Added UC2 :Render OOPS as Banner using Print Statements

--- src/BannerApp.java
+++ src/BannerApp.java
@@ -1,5 +1,38 @@
 public class BannerApp {
     public static void main(String[] args){
-        System.out.println("OOPS");
+        // Line 1
+        System.out.println(
+                "  *****  " + "  *****  " + " ******  " + "  *****  "
+        );
+
+        // Line 2
+        System.out.println(
+                " *     * " + " *     * " + " *     * " + " *        "
+        );
+
+        // Line 3
+        System.out.println(
+                " *     * " + " *     * " + " *     * " + " *        "
+        );
+
+        // Line 4
+        System.out.println(
+                " *     * " + " *     * " + " ******  " + "  *****  "
+        );
+
+        // Line 5
+        System.out.println(
+                " *     * " + " *     * " + " *        " + "       * "
+        );
+
+        // Line 6
+        System.out.println(
+                " *     * " + " *     * " + " *        " + "       * "
+        );
+
+        // Line 7
+        System.out.println(
+                "  *****  " + "  *****  " + " *        " + "  *****  "
+        );
     }
 }
